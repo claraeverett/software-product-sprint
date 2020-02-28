e53a4a8bd8d688230f97a0bee1954a6c2d7c4056
Clara <everettclara8@gmail.com>
2020-02-27 21:49:31 -0800

    return hard coded comments- PR 3

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -19,14 +19,31 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
+import com.google.gson.Gson;
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
-
+  private List<String> messages;
+  @Override
+  public void init(){
+      messages = new ArrayList<>();
+      messages.add("Bonjour");
+      messages.add("Ciao");
+      messages.add("Hola");  
+  }
+  
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
     response.getWriter().println("<h1>Hello Clara!</h1>");
+
+    Gson gson = new Gson();
+    String json = gson.toJson(messages);
+
+    response.setContentType("text/html;");
+    response.getWriter().println(json);
   }
 }
